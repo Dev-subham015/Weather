f542fddfdb4066fe8665855505891e02a60e7f3c
Dev-subham015 <devlpersubham@gmail.com>
2024-08-12 02:26:19 +0530

    Update settings.py for PythonAnywhere deployment

--- weather/settings.py
+++ weather/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@33^l9f8t=k&4d-^@7rlig!df_*#q-(-ckq8ay(0n#04iq5i^s'
+import os
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-@33^l9f8t=k&4d-^@7rlig!df_*#q-(-ckq8ay(0n#04iq5i^s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
